a81a5cd7c71d115b4fef9b8bc1e5eff0fea0ee58
Kazi Solayman <kazisolayman123@gmail.com>
2025-01-03 00:14:01 +0600

    included gender in registration form

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -12,7 +12,7 @@ const Register = () => {
     })
     const handleSubmitForm = (e) => {
         e.preventDefault();
-
+        console.log(user)
         setUser({
             fullName: "",
             username: "",
@@ -21,7 +21,7 @@ const Register = () => {
             gender: "",
         })
     }
-    console.log(user)
+    
     return (
         <div className='flex justify-center items-center h-screen'>
             <div className='w-96 border rounded-md p-5'>
@@ -42,6 +42,16 @@ const Register = () => {
                     <div className='flex flex-col gap-1 mb-4'>
                         <label htmlFor='cpassword' className='text-sm '>Confirm Password</label>
                         <input type="password" placeholder="Enter full name" id="cpassword" name="cpassword" value={user.confirmPassword} onChange={(e) => setUser({ ...user, confirmPassword: e.target.value })} className='outline-none border rounded-md p-2 text-sm' required />
+                    </div>
+                    <div className='flex items-center gap-5 mb-4'>
+                        <div className='flex items-center gap-2'>
+                            <input type="radio" id="male" name="gender" value="Male" checked={ user.gender === "Male" } onChange={(e) => setUser({ ...user, gender: e.target.value })} />
+                            <label htmlFor='male' className='text-sm'>Male</label>
+                        </div>
+                        <div className='flex items-center gap-2'>
+                            <input type="radio" id="female" name="gender" value="Female" checked={ user.gender === "Female" } onChange={(e) => setUser({ ...user, gender: e.target.value })} />
+                            <label htmlFor='female' className='text-sm'>Female</label>
+                        </div>
                     </div>
                     <p className='text-sm text-center '>Already have an account? <Link href="/login" className='underline'>Login</Link></p>
                     <button type="submit" className='mt-4 bg-teal-400 rounded-md w-full h-11 text-white font-semibold'>Register</button>
